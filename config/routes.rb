5090c73cbfb4d779f8cc45e94b8181958a0c672c
aaronkoz74 <aaronkoz74@gmail.com>
2016-01-05 18:23:22 -0500

    Added welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  get 'welcome/index'
+  root to: 'welcome#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root to: 'welcome#index'
-  get '/welcome' => 'welcome#index', :as => 'user_root'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
