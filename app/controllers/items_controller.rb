73cd07f3c2d216bd1c6d66db266c6383859e13ad
aaronkoz74 <aaronkoz74@gmail.com>
2016-01-12 23:20:24 -0500

    Incorporate ajax - in-process

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :only_permit_current_user, only: [:new, :create, :destroy]
 
-
   def new
     @user = User.find(params[:user_id])
     @item = Item.new
@@ -21,18 +20,20 @@ class ItemsController < ApplicationController
 
   def destroy
     @user = User.find(params[:user_id])
-    @item = Item.find(params[:id])
+    @item = @user.items.find(params[:id])
 
     if @item.destroy
       flash[:notice] = "Item was removed from your list - Congratulations!"
 
     else
-      flash[:alert] = "Item couldn't be removed from your list.  Try again."
+      flash[:alert] = "Item could not be removed from your list.  Try again."
     end
 
     respond_to do |format|
-      format.html
-      format.js
+      format.html do
+        redirect_to root_url
+      end
+      format.js #{ render js: 'destroy.js.erb' }
     end
   end
 
@@ -45,8 +46,16 @@ class ItemsController < ApplicationController
   def only_permit_current_user
     @user = User.find(params[:user_id])
     unless user_signed_in? && current_user == @user
-      redirect_to root_path
-      flash[:notice] = "You must be logged in as user to add or delete items."
+      respond_to do |format|
+        format.html do
+          redirect_to user_path(@user), status: 303
+          flash[:notice] = "You must be logged in as that user to add or delete items."
+        end
+        format.js do
+          flash.now[:notice] = "You must be logged in as that user to add or delete items."
+          # render js file with a flash alert saying
+        end
+      end
     end
   end
 end
