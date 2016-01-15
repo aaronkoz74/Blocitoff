ff57a8efc97ddc25af38ea15a6a2565a75d6c05e
aaronkoz74 <aaronkoz74@gmail.com>
2016-01-15 10:44:07 -0500

    Delete expired in-process -- fixing time remaining

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessor :task
+  attr_accessor :task, :time_remaining
+
+  def time_remaining(entry)
+    limit = 7.days
+    span = distance_of_time_in_words(entry.created_at, Time.now)
+
+    if span <= limit
+      limit - span
+    else
+      "Overdue!"
+    end
+  end
+
+
 end
